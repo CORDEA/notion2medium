33caeabdad2fb836085931a7b8467d7111bca44f
Yoshihiro Tanaka <contact@cordea.jp>
2021-12-31 14:01:14 +0900

    create gists

--- source/app.d
+++ source/app.d
@@ -1,9 +1,12 @@
 import std.stdio;
+import std.string;
 import std.file;
 import std.path;
 import std.array;
 import std.format;
 import std.algorithm.searching;
+import std.process;
+import std.net.curl;
 import std.json;
 
 void main(string[] args)
@@ -17,24 +20,43 @@ void main(string[] args)
     {
         return;
     }
+    auto token = environment.get("GITHUB_TOKEN");
+    if (token is null)
+    {
+        return;
+    }
     auto content = readText(filename);
     auto parsed = parseContent(content.split());
     foreach (Content c; parsed)
     {
         if (Code code = cast(Code) c)
         {
-            createGist(code, filename, "");
+            createGist(code, filename, token);
         }
     }
 }
 
-void createGist(Code code, string filename, string token)
+bool createGist(Code code, string filename, string token)
 {
+    auto http = HTTP();
+    http.method = HTTP.Method.post;
+    http.url = "https://api.github.com/gists";
+    http.addRequestHeader("Authorization", "token %s".format(token));
+    http.addRequestHeader("Accept", "application/vnd.github.v3+json");
+
     auto name = filename.baseName().setExtension(code.langCode() is null ? "txt" : code.langCode());
     JSONValue file = JSONValue([
         name: JSONValue(["content": code.values().join("\n")])
     ]);
     JSONValue json = JSONValue(["files": file]);
+    http.setPostData(json.toString(), "application/json");
+
+    HTTP.StatusLine status;
+    http.onReceiveStatusLine = (l) { status = l; };
+    http.perform();
+
+    writeln(status);
+    return status.code == 201;
 }
 
 Content[] parseContent(string[] lines)
